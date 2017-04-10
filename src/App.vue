29cfd5d8fc95b38486a1e5adc45c736f3464bc92
helderdiin <helderdiin.work@gmail.com>
2017-04-10 13:27:45 -0300

    Ajustando navbar da aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="/home">
-            <img alt="Brand" src="./assets/images/logo.png">
-          </a>
+      <div class="navbar__container-fluid row">
+        <div class="col-xs-offset-1 col-xs-10">
+          <div class="container-fluid__logo-wrapper col-xs-2">
+            <a class="container-fluid__logo" href="/home">
+              <img alt="Brand" src="./assets/images/logo.png">
+            </a>
+          </div>
+          <div class="container-fluid__menu col-xs-offset-8 col-xs-2">
+            <div class="pull-right">
+              <a href="javascript:void(0);">
+                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+              </a>
+              <a href="javascript:void(0);">
+                <span>Menu</span>
+                <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
+
     <router-view></router-view>
   </div>
 </template>
@@ -22,7 +36,7 @@ export default {
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Ubuntu');
 
-#app {
+html {
   font-family: 'Ubuntu', sans-serif;
 }
 
@@ -32,9 +46,25 @@ export default {
   border: 0px;
 }
 
-.navbar-brand {
-  width: 200px;
-  padding: 7px;
+.container-fluid__menu {
+  line-height: 50px;
+  font-size: 17px;
+  & a {
+    color: #fff;
+    text-decoration: none;
+    &:first-child {
+      margin-right: 20px;
+    }
+  }
+}
+
+.container-fluid__logo-wrapper {
+  padding: 8px;
+}
+
+.container-fluid__logo {
+  padding: 0px;
+  height: auto;
   & img {
     width: 100%;
   }
