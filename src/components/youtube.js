3ec49c357cad336a20baa79b356ee04bfe22659b
Helder Traci <helderdiin.work@gmail.com>
2017-04-12 10:18:52 -0300

    Criando componente para comunicação com API do YouTube

---
+++ src/components/youtube.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { APP } from '../constants';
+import { objToQueryParam } from '../utils';
+
+const request = (config) => {
+  return axios(config);
+};
+
+class Youtube {
+  constructor() {
+    this.url = `${APP.YOUTUBE.URL}/playlistItems?key=${APP.YOUTUBE.KEY}&playlistId=${APP.YOUTUBE.PLAYLIST_ID}`;
+    this.defaultParams = {
+      part: 'snippet,contentDetails',
+      maxResults: 50,
+    };
+  }
+
+  getVideos(params = {}) {
+    const defaultParams = Object.assign({}, this.defaultParams, params);
+    const url = `${this.url}&${objToQueryParam(defaultParams)}`;
+
+    return request({ url });
+  }
+}
+
+export default new Youtube();
