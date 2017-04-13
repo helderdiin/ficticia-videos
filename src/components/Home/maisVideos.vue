7091dc6f5ebc6d5cf3b0216b22348a6556497ed5
Helder Traci <helderdiin.work@gmail.com>
2017-04-13 20:27:24 -0300

    Refatoração no componente de + vídeos e no vídeo preview
    - Criado componente de + vídeos
    - Criado componente para preview de vídeo

---
+++ src/components/Home/maisVideos.vue
@@ -0,0 +1,251 @@
+<template>
+  <div class="mais-videos">
+    <div class="mais-videos__lista-videos">
+      <div class="lista-videos__titulo">
+        <h2>+ Vídeos</h2>
+      </div>
+      <div class="lista-videos__conteudo">
+        <div class="conteudo__items-wrapper style-scroll">
+          <video-preview :videos="videos" @updateSelectedVideo="setSelectedVideo"></video-preview>
+        </div>
+        <div class="conteudo__carregar-mais">
+          <div class="carregar-mais__botao" :class="{ 'carregar-mais__botao--invisible': carregandoMaisVideos }" @click="carregarMaisVideos">
+            <span> carregar mais vídeos... </span>
+          </div>
+          <div class="carregar-mais__loading" :class="{ 'carregar-mais__loading--visible': carregandoMaisVideos }">
+            <span class="glyphicon glyphicon-refresh gly-spin"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoPreview from '../videoPreview';
+
+import youtube from '../youtube';
+import { backSlashToBreakLine, normalizeTimeNumber } from '../../utils';
+import { APP } from '../../constants';
+
+const getBestThumbnail = (thumbnails = {}) => {
+  if (thumbnails.maxres) {
+    return thumbnails.maxres.url;
+  } else if (thumbnails.high) {
+    return thumbnails.high.url;
+  }
+  return thumbnails.default.url;
+};
+
+const getGroups = (duration = '') => {
+  const regexpMS = /PT(\d+)M(\d+)S/;
+  const regexpM = /PT(\d+)M/;
+  const regexpS = /PT(\d+)S/;
+
+  if (duration.match(/[M][S]/g)) {
+    return regexpMS.exec(duration);
+  } else if (duration.match(/[M]/g)) {
+    return regexpM.exec(duration);
+  }
+
+  return regexpS.exec(duration);
+};
+
+const getVideoDuration = (duration = '') => {
+  const groups = getGroups(duration);
+
+  return `${normalizeTimeNumber(groups[1])}:${normalizeTimeNumber(groups[2])}`;
+};
+
+const getShortViews = (views = 0) => {
+  return views > 1000 ? `${parseInt((+views / 1000), 10)}k` : views;
+};
+
+const getVideosObject = (items = []) => {
+  return items.map((i) => {
+    return {
+      url: `${APP.YOUTUBE.EMBED_URL}/${i.id}`,
+      title: i.snippet.title,
+      description: backSlashToBreakLine(i.snippet.description),
+      thumbnail: getBestThumbnail(i.snippet.thumbnails),
+      views: {
+        full: i.statistics.viewCount,
+        short: getShortViews(i.statistics.viewCount),
+      },
+      duration: getVideoDuration(i.contentDetails.duration),
+    };
+  });
+};
+
+export default {
+  name: 'mais-videos',
+  components: { VideoPreview },
+  methods: {
+    getVideos: function getVideos() {
+      youtube.getVideos({
+        maxResults: 4,
+      }).then((items = []) => {
+        this.videos = [...this.videos, ...getVideosObject(items)];
+        this.setSelectedVideo(this.videos[0]);
+      });
+    },
+    carregarMaisVideos: function carregarMaisVideos() {
+      this.toggleCarregandoMaisVideos();
+
+      youtube.getMoreVideos({
+        maxResults: 4,
+      }).then((items = []) => {
+        this.toggleCarregandoMaisVideos();
+        this.videos = [...this.videos, ...getVideosObject(items)];
+      });
+    },
+    toggleCarregandoMaisVideos: function toggleCarregandoMaisVideos() {
+      this.carregandoMaisVideos = !this.carregandoMaisVideos;
+    },
+    setSelectedVideo: function setSelectedVideo(video = {}) {
+      this.$emit('updateSelectedVideo', video);
+    },
+  },
+  created() {
+    this.getVideos();
+  },
+  data() {
+    return {
+      videos: [],
+      carregandoMaisVideos: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.lista-videos__conteudo {
+  margin-bottom: 30px;
+  padding: 15px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  margin-top: 30px;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.25);
+}
+
+.lista-videos__titulo {
+  height: auto;
+  margin-top: 30px;
+}
+
+.conteudo__item {
+  height: 120px;
+  margin-bottom: 15px;
+  position: relative;
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.item__infos {
+  padding: 0 5px;
+  height: 100%;
+}
+
+.item__img {
+  padding: 0;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+  & img {
+    width: 100%;
+    max-height: 120px;
+  }
+}
+
+.infos__views {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.4);
+  font-weight: 700;
+}
+
+.views__icon {
+  float: left;
+  font-size: 20px;
+}
+
+.infos__text {
+  float: left;
+  margin-left: 5px;
+  line-height: 22px;
+}
+
+.infos__titulo {
+  width: 100%;
+  height: 80px;
+  & h4 {
+    display: -webkit-box;
+    margin: 0 auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+  }
+}
+
+.conteudo__carregar-mais {
+  text-align: center;
+  color: rgba(0, 0, 0, 0.5);
+  margin-top: 15px;
+}
+
+.carregar-mais__botao {
+  width: 100%;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  padding: 5px 0;
+  border-radius: 5px;
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: #fff;
+  }
+  & span {
+    text-transform: uppercase;
+    font-size: 13px;
+    font-weight: 700;
+  }
+}
+
+.carregar-mais__loading {
+  display: none;
+  padding: 5px 0;
+  border: 1px solid rgba(0, 0, 0, 0);
+}
+
+.carregar-mais__loading--visible {
+  display: block;
+}
+
+.carregar-mais__botao--invisible {
+  display: none;
+}
+
+.conteudo__items-wrapper {
+  height: 525px;
+  overflow-x: hidden;
+}
+
+.img__duration {
+  color: #fff;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 17%;
+  font-weight: 700;
+  font-size: 10px;
+  padding: 1px 3px;
+  background-color: rgba(0, 0, 0, 0.8);
+  text-align: center;
+  margin: 0 4px 4px 0;
+  border-radius: 5px;
+}
+
+</style>
---
+++ src/components/videoPreview.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="video-preview">
+    <div class="conteudo__item" v-for="video in videos" @click="setSelectedVideo(video)">
+      <div class="item__img col-xs-7">
+        <img :src="video.thumbnail" />
+        <span class="img__duration"> {{video.duration}} </span>
+      </div>
+      <div class="item__infos col-xs-5">
+        <div class="infos__titulo">
+          <h4> {{video.title}} </h4>
+        </div>
+        <div class="infos__views">
+          <div class="views__icon">
+            <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+          </div>
+          <div class="infos__text">
+            <span> {{video.views.short}} views </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'video-preview',
+  props: {
+    videos: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  methods: {
+    setSelectedVideo: function setSelectedVideo(video = {}) {
+      this.$emit('updateSelectedVideo', video);
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.conteudo__item {
+  height: 120px;
+  margin-bottom: 15px;
+  position: relative;
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.item__img {
+  padding: 0;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+  & img {
+    width: 100%;
+    max-height: 120px;
+  }
+}
+
+.img__duration {
+  color: #fff;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 17%;
+  font-weight: 700;
+  font-size: 10px;
+  padding: 1px 3px;
+  background-color: rgba(0, 0, 0, 0.8);
+  text-align: center;
+  margin: 0 4px 4px 0;
+  border-radius: 5px;
+}
+
+.item__infos {
+  padding: 0 5px;
+  height: 100%;
+}
+
+.infos__titulo {
+  width: 100%;
+  height: 80px;
+  & h4 {
+    display: -webkit-box;
+    margin: 0 auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+  }
+}
+
+.infos__views {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.4);
+  font-weight: 700;
+}
+
+.views__icon {
+  float: left;
+  font-size: 20px;
+}
+
+.infos__text {
+  float: left;
+  margin-left: 5px;
+  line-height: 22px;
+}
+
+</style>
